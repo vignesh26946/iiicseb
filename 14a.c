3cd7b65c040aed2da58706a5e60bcc10b0cdb830
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-01-25 16:28:06 +0530

    Create 14a.c

---
+++ 14a.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,c;
+  prinrf("\n enter the number");
+  scanf("%d%d",&a,&b);
+  for(c=2;i<6;i++)
+  {
+    if((i<2)!=0)
+      printf("\n a odd number%d",c);
+    }
+}
