cf4b030a884b513d44fc5ab6ea27dae75aa330d3
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-01-25 16:46:37 +0530

    Create 18a.c

---
+++ 18a.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main()
+{
+int n,r,sum=0,i,o,b,s;
+printf("\nEnter the initial value: ");
+scanf("%d",&o);
+printf("\nEnter the end value: ");
+scanf("%d",&b);
+for(i=a;i<=b;i++)
+{
+s=i;
+while(t>0)
+{
+r=t%10;
+sum=sum+(r*r*r);
+s=s/10;
+}
+if(i==sum)
+{
+printf("\n%d\n",i);	}
+sum=0;
+}
+}
