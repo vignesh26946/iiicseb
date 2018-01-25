c63a8127f724ac3906cfa241bf46dbe6e7e72a16
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-01-25 16:23:50 +0530

    Create 13a.c

---
+++ 13a.c
@@ -0,0 +1,18 @@
+#inclufde<stdio.h>
+int main(){
+int v;main=0;i=0;
+printf("enter number");
+scanf("%d",&n);
+for(i=2;i<v;i++)
+{
+if(v% i==0)
+num =num+1
+break;
+}
+if(num==1)
+printf("It is not prime number");
+else
+printf("It is not prime number");
+}
+return o;
+}
