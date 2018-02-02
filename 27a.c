30fb4fb11f2c1f53ed1148ac9262851fa5cac0a7
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-02-02 08:55:15 +0000

    Create 27a.c

---
+++ 27a.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int j,flag=0,n=0;
+clrscr();
+printf("\n enter a string");
+scanf("%d",s);
+for(j=0;s[j]!=0;j++)
+{
+if(s[j]='0';
+{
+flag=flag+1;
+}
+}
+if(flag==0)
+printf("\n string is not contain num"):
+else
+printf("\n string is contain num");
+return 0;
+}
