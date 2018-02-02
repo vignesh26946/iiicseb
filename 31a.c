749a0e5fea79e3d831ae31a4db66f5fd01bb69eb
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-02-02 09:03:33 +0000

    Create 31a.c

---
+++ 31a.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n,n1,n2,n3;
+    char a[10],b[10],s[100];
+    scanf("%c %c %c",&a,&b,&c);
+    n1=strlen(a);
+    n2=strlen(b);
+    n3=strlen(s);
+    n=n1+n2+n3;
+    printf("%d",n);
+    return 0;
+}
+
+
