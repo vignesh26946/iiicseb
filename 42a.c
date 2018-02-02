270611c8335aed846c950aca6727414ad5358fa5
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-02-02 09:31:15 +0000

    Create 42a.c

---
+++ 42a.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{
+  char h[50],y[50];
+  int i,temp=0;
+  printf("\nEnter the first string");
+  gets(h);
+  printf("\nEnter the second string");
+  gets(y);
+  for(i=0;x[i]!='\0';i++)
+  {
+  if(h[i]==y[i])
+  temp=1;
+  else
+  temp=0;
+  }
+  if(temp==1)
+  printf("\nThe sting is:%s",h[i]);
+  else
+  printf("\n The greater sting is:%s",y[i]);
+} 
