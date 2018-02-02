52eeb45d06ee53991db7bb575635ae4cc1c36625
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-02-02 09:09:45 +0000

    Create 34a.c

---
+++ 34a.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+   char str[100];
+  int count=0;
+  printf("\n Enter the string");
+  scanf("%d",&str);
+  while(str[k]=='\0')
+  {
+ 
+ if(str[k]=='.')
+  {
+    count++;
+   }
+   i++;
+}
