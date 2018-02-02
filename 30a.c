c5fb2eb257b9fb31e352f12924deb720d8bff927
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-02-02 09:00:56 +0000

    Create 30a.c

---
+++ 30a.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int k,j,l,m,p,g;
+printf("enter the first time: ");
+scanf("%d%d",&k,&j);
+printf("enter the second time: ");
+scanf("%d%d",&l,&m);
+p=k-l;
+g=j-m;
+printf("the time is %d%d",p,f);
+return 0;
+}
