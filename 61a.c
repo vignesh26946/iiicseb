59c5a6a72b6e164ef319e2453b54efc618019ea5
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-02-08 14:53:00 +0530

    Create 61a.c

---
+++ 61a.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void)
+{
+  char a[30];
+  int k,i;
+  scanf("%a",a);
+  scanf("%d",&k);
+  for(i=0;i<k;i++)
+  {
+    printf("%c",a[i]);
+  }
+  return 0;
+}
