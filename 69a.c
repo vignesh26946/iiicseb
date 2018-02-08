9789eb24e8f63a822a9a5cd3f9aeb17f0f0887de
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-02-08 15:07:27 +0530

    Create 69a.c

---
+++ 69a.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	int num2,num2,diff;
+            scanf("%d%d",&num2,&num2);
+            diff=num2-num2;
+            if(diff%2==0)
+            printf("Even");
+            else
+	printf("Odd");
+return 0;
