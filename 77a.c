3ddd0822b0a1345af5b6b056b54d84bb4426b450
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-02-09 14:19:09 +0530

    Create 77a.c

---
+++ 77a.c
@@ -0,0 +1,18 @@
+
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class fa{
+     public static void main(String aa[]){
+       int s;
+        Scanner s=new Scanner(System.in);
+        b=s.nextInt();
+        for(int i=1;i<=s;i++){
+            if(s%i==0){
+                System.out.print(i);
+            }
+        }
+      
+  
+}
+}
