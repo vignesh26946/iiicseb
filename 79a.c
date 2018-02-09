62f8153816e5db6666834de77207d64e5a84acab
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-02-09 14:22:31 +0530

    Create 79a.c

---
+++ 79a.c
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class sqrt{
+     public static void main(String aa[]){
+       int s;
+        Scanner s=new Scanner(System.in);
+       s=s.nextInt();
+      int c=s.nextInt();
+      int h=s*c;
+      double j=Math.sqrt(h);
+      if(j==c){
+          System.out.println("Yes");
+      }
+ else{
+          System.out.println("No");
+ }
+}
+}
