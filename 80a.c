f51bb9b0c40430fdba46ff23943f5c11ea421e38
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-02-09 14:24:54 +0530

    Create 80a.c

---
+++ 80a.c
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class odds{
+     public static void main(String aa[]){
+       int s;
+      Scanner s=new Scanner(System.in);
+      s=s.nextInt();
+      int a[]=new int[b];
+      for(int i=0;i<s;i++){
+          a[i]=s.nextInt();
+      }
+      for(int i=0;i<a.length;i++){
+          if(a[i]%2!=0){
+              System.out.printf("%d ",a[i]);
+          }
+      }
+}
+}
