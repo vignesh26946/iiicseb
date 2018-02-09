8fb46442b563de2c17b737ff37ec88e73bd54c22
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-02-09 14:14:06 +0530

    Create 74a.c

---
+++ 74a.c
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class rou{
+     public static void main(String aa[]){
+        float k;
+        Scanner s=new Scanner(System.in);
+        k=s.nextFloat();
+      int v=  Math.round(k);
+      System.out.println(v);
+  
+}
+}
