a2f396cbba863fc811b4aaa5cb82e0f57a9ca3db
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-02-09 14:56:14 +0530

    Create 93a.c

---
+++ 93a.c
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class kelv{
+
+     public static void main(String aa[]){
+    int s,b,c;
+    Scanner s=new Scanner(System.in);
+    s=s.nextInt();
+    double n=as+273.5;
+    System.out.println(n);
+
+           
+   }
+}
