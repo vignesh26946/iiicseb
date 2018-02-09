199abcb89b6f5067f959ff2d981af834aba8709e
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-02-09 14:20:32 +0530

    Create 78a.c

---
+++ 78a.c
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class po{
+     public static void main(String aa[]){
+       int s;
+        Scanner s=new Scanner(System.in);
+       s=s.nextInt();
+       if(s%13==0){
+           System.out.println("Yes");
+       }
+ else{
+           System.out.println("No");
+ }
+  
+}
+}
