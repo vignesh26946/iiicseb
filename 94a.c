5b7b9c6d1141f56382cfd5c7c051befbcca5f546
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-02-09 14:57:44 +0530

    Create 94a.c

---
+++ 94a.c
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class dd{
+
+     public static void main(String aa[]){
+    int s,b,c;
+    Scanner s=new Scanner(System.in);
+    s=s.nextInt();
+    b=s.nextInt();
+    for(int i=1;i<=s;i++){
+        if(b==i){
+            System.out.println(i);
+        }
+    }
+
+           
+   }
+}
