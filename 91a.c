bd78d2f655451efcf7c28dc8e83aa805f32f7fb3
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-02-09 14:52:58 +0530

    Create 91a.c

---
+++ 91a.c
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class surfv {
+
+     public static void main(String aa[]){
+    int s,b,c;
+    int vol=0;
+    int suf=0;
+    Scanner s=new Scanner(System.in);
+    s=s.nextInt();
+    b=s.nextInt();
+    c=s.nextInt();
+    suf=2*(s*b)+(b*c)+(c*s);
+    vol=a*b*c;
+    System.out.println(suf);
+     System.out.println(vol);
+           
+   }
+}
