c0bf5c8f0da7641809336328512172c384572402
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-02-09 14:59:04 +0530

    Create 95a.c

---
+++ 95a.c
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class callk{
+
+     public static void main(String aa[]){
+    int s,b,c;
+    Scanner s=new Scanner(System.in);
+    s=s.nextInt();
+    b=s.nextInt();
+    c=s.nextInt();
+    int p=(s*b*c)/100;
+    System.out.println(p);
+
+           
+   }
+}
