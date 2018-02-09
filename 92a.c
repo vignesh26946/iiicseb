4e8b4b306e3a37d1f3a0f5a3bb06cce0c8c755a5
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-02-09 14:54:40 +0530

    Create 92a.c

---
+++ 92a.c
@@ -0,0 +1,19 @@
+import java.util.*;
+public class summm{
+      public static void main(String aa[]){
+            int s;
+            int a;
+            int sum=0;
+            Scanner s=new Scanner(System.in);
+            a=s.nextInt();
+            n=s.nextInt();
+            int b[]=new int[n];
+            for(int i=0;i<s;i++){
+                b[i]=s.nextInt();
+            }
+            for(int i=0;i<=a;i++){
+             sum=sum+b[i];
+            }
+             System.out.println(sum);
+    }
+}
