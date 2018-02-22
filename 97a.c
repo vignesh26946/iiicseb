6c52c1b4b8a569a0b2a12ded4efb26fd5ef10dce
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-02-22 16:03:26 +0530

    Create 97a.c

---
+++ 97a.c
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class rev1 {
+
+     public static void main(String aa[]){
+    int s,b,c;
+    Scanner s=new Scanner(System.in);
+   s=s.nextInt();
+   String n=String.valueOf(a);
+   String z=new StringBuffer(n).reverse().toString();
+   System.out.println(z);
+
+           
+   }
+}
