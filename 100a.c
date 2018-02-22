44e15b2fb7ece1176e94b52569d8ee918534c82c
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-02-22 16:21:58 +0530

    Create 100a.c

---
+++ 100a.c
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class mod{
+
+     public static void main(String aa[]){
+    int s,b,c;
+    int h1;
+    Scanner s=new Scanner(System.in);
+    s=s.nextInt();
+   b=s.nextInt();
+   c=s.nextInt();
+   h1=(s*b)%c;
+   System.out.println(h1);
+   
+           
+   }
+}
