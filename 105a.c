c4c26dbeed7e8136e60005891ff069176ed7a785
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-03-01 15:59:25 +0530

    Create 105a.c

---
+++ 105a.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int num2,num1,power;
+ printf("enter the number:");
+ scanf("%d\n%d\n",&num2,&num1);
+ power=pow(num2,num1);
+ printf("the power of given two number is %d",power);
+ getch();
+ return 0;
+ }
+© 2018 GitHub, Inc.
