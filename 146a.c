7ce2a40dbf2a6d70273ef1c4deb8decc82c1ed9e
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-03-13 16:22:57 +0530

    Create 146a.c

---
+++ 146a.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main(void) 
+{
+	char str[20];
+	int i;
+	scanf("%[^\n]s",str);
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(str[i]==' ')
+		{
+			str[i]='+';
+		}
+	}
+	for(i=0;str[i]!='\0';i++)
+	{
+	if(str[i]!='+')
+	{
+	printf("%c",str[i]);
+	}
+	}
+	return 0;
+}
