3af738f30bbb167b5de6f6f969507e4c4baf590b
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-03-20 16:25:11 +0530

    Create 151a.c

---
+++ 151a.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+ int main()
+{
+int water=0's,land=1's;
+char x;
+printf("the value of water");
+printf("the value of land");
+do
+{
+switch(x=getchar()
+{
+case '1':printf("edge of water in south");
+         break;
+case '2':printf("edge of the water in north");
+          breake;
+case '3':printf("edge of the water in weast"); 
+         break;
+case '4':printf("edge of the water in east"):
+          break;
+defult:
+i++:
+break;
+}
+}
+while(x1="\n");
+printf("no.of edge in water is%d\n,0's);
+printf("no.of edge in land is?%d\n,1's);
+getch();
+}
